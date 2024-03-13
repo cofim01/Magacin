6ea6da706ef451a99b756fc802ef1b173ca4218e
Filip Mrdak <fmrdak@gmail.com>
2024-03-13 20:19:23 +0100

    Dodata implementacija jedne metode u okviru main grane

--- Magacin/src/artikli/Magacin.java
+++ Magacin/src/artikli/Magacin.java
@@ -1,12 +1,23 @@
 package artikli;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.interfejs.MagacinInterface;
 
 public class Magacin implements MagacinInterface {
-
+	List<Artikal> artikli=new ArrayList<>();
 	@Override
 	public void dodajArtikal(Artikal a) {
-		// TODO Auto-generated method stub
+		if(artikli.contains(a)) {
+			for(Artikal artikal:artikli) {
+				if(artikal.equals(a)) {
+					artikal.setKolicina(artikal.getKolicina()+1);
+					return;
+				}
+			}
+		}
+		artikli.add(a);
 		
 	}
 
