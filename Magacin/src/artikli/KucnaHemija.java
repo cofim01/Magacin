6d7af1a513901963a4125a69ab5d371331b13127
Filip Mrdak <fmrdak@gmail.com>
2024-03-13 11:20:54 +0100

    Izmenjene klase za magacin

--- Magacin/src/artikli/KucnaHemija.java
+++ Magacin/src/artikli/KucnaHemija.java
@@ -2,38 +2,11 @@ package artikli;
 
 import java.time.LocalDate;
 import java.util.Objects;
-import java.util.Set;
 
-public class KucnaHemija {
-	private String naziv;
-	private int sifra;
-	private String opis;
-	private int kolicina;
+
+public class KucnaHemija extends Artikal {
 	private LocalDate rokTrajanja;
-	public String getNaziv() {
-		return naziv;
-	}
-	public void setNaziv(String naziv) {
-		this.naziv = naziv;
-	}
-	public int getSifra() {
-		return sifra;
-	}
-	public void setSifra(int sifra) {
-		this.sifra = sifra;
-	}
-	public String getOpis() {
-		return opis;
-	}
-	public void setOpis(String opis) {
-		this.opis = opis;
-	}
-	public int getKolicina() {
-		return kolicina;
-	}
-	public void setKolicina(int kolicina) {
-		this.kolicina = kolicina;
-	}
+	
 	public LocalDate getRokTrajanja() {
 		return rokTrajanja;
 	}
