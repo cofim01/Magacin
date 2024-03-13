b8764cce0a78bc4e1fbecdbe38e436b1b2e1fdc1
Filip Mrdak <fmrdak@gmail.com>
2024-03-13 20:05:18 +0100

    Dodata klasa magacin i sve metode iz interfejsa

---
+++ Magacin/src/artikli/Magacin.java
@@ -0,0 +1,25 @@
+package artikli;
+
+import magacin.interfejs.MagacinInterface;
+
+public class Magacin implements MagacinInterface {
+
+	@Override
+	public void dodajArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pronadjiArtikal(Artikal a) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
